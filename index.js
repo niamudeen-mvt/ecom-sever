264b5120c2d97e628f6042baebb090c0169feb50
niamudeen-mvt <niamudeen.mvteams@gmail.com>
2024-08-01 11:42:16 +0530

    feat - implement cron jobs in node js

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const express = require("express");
 const cors = require("cors");
 const router = require("./router/index.js");
 const connectDb = require("./utils/db.js");
+const cron = require("node-cron");
 connectDb();
+
+const TIME_EXPRESSION = "0 9 * * *";
+// const TIME_EXPRESSION = "* * * * * *";
 
 const PORT = process.env.PORT || 7000;
 
@@ -15,7 +19,15 @@ app.use(cors());
 app.use("/api/v1", router);
 
 app.get("/", (req, res) => {
-  res.send("ecom backend server is working");
+  res.json("ecom backend server is working");
+});
+
+cron.schedule(TIME_EXPRESSION, () => {
+  fetch("http://localhost:7000/")
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+    });
 });
 
 app.listen(PORT, () => {
